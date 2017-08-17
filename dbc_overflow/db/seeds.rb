8175feb3fa375f0f71dcaaffed0e4a4730793bf5
Hannie Dong <hdong92@gmail.com>
2017-08-17 11:56:56 -0700

    Modify seed file.

--- dbc_overflow/db/seeds.rb
+++ dbc_overflow/db/seeds.rb
@@ -5,11 +5,13 @@ require 'faker'
   user1 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password_hash: 'password')
   user2 = User.create(username: Faker::Name.name, email: Faker::Internet.email, password_hash: 'password')
 
-  # first user asks question
+  # user asks question
   user1.questions.create(title: "#{user1.username} Question Title", body: "#{user1.username} Question Body")
-
+  user2.questions.create(title: "#{user2.username} Question Title", body: "#{user2.username} Question Body")
   # second user answers that question
-  user2.answers.create(body: "#{user2.username} Answer Body", question: Question.last
+  user2.answers.create(body: "#{user2.username} Answer Body", question: Question.first)
+  user1.answers.create(body: "#{user1.username} Answer Body", question: Question.last)
+
 
 users = User.all
 questions = Question.all
