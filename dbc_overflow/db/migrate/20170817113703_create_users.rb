7f84c10bd81cff0d8fb585f740cc8f66dba5502c
nickell75 <nickell75@yahoo.com>
2017-08-17 12:28:01 -0700

    fixes bugs

--- dbc_overflow/db/migrate/20170817113703_create_users.rb
+++ dbc_overflow/db/migrate/20170817113703_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username, null: false
       t.string :email, null: false
-      t.string :passwor_hash, null: false
+      t.string :password_hash, null: false
 
       t.timestamps
     end
