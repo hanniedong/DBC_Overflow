951e76bcec1b316db3486a2c6411d589863b77f0
Nestor Ceron <nestor_ceron@yahoo.com>
2017-08-18 17:38:10 -0700

    Chage post route for questions and added hidden user field

--- dbc_overflow/app/controllers/questions_controller.rb
+++ dbc_overflow/app/controllers/questions_controller.rb
@@ -21,22 +21,22 @@ get '/questions/:id' do
   upvotes = @question.votes.where(up_vote:1)
   downvotes = @question.votes.where(down_vote:1)
   @total_votes = upvotes.count - downvotes.count
-  p current_user
-  p @user = User.find_by(id: session[:user_id])
-  p @answer = @question.answers.find_by(id: @question.id)
-  p @user_of_answer = User.find_by(id: @answer.user_id)
+  # p current_user
+  # @user = User.find_by(id: session[:user_id])
+  # p @answer = @question.answers.find_by(id: @question.id)
+  # p @user_of_answer = User.find_by(id: @answer.user_id)
   erb :'/questions/show:vote'
 end
 
 post '/questions/:id/upvote' do
   @question = Question.find(params[:id])
   @user_vote = @question.votes.find_by(user_id:session[:user_id])
-  if @user_vote != nil 
+  if @user_vote != nil
     @user_vote.destroy!
-  end 
+  end
     Vote.create(user_id: session[:user_id], votable_type: "Question", votable_id: @question.id, up_vote: 1)
 
-  
+
 	redirect "/questions/#{@question.id}"
 
   # if request.xhr?
@@ -49,9 +49,9 @@ end
 post '/questions/:id/downvote' do
   @question = Question.find(params[:id])
   @user_vote = @question.votes.find_by(user_id:session[:user_id])
-  if @user_vote != nil 
+  if @user_vote != nil
     @user_vote.destroy!
-  end 
+  end
   Vote.create(user_id: session[:user_id], votable_type: "Question", votable_id: @question.id, down_vote: 1)
   @question.votes.to_s
   redirect "/questions/#{@question.id}"
