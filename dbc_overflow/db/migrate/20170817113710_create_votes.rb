7b047207b27f215aa17f83724c1bec87ebaefb22
Hannie Dong <hdong92@gmail.com>
2017-08-18 17:11:05 -0700

    Add upvote and downvote function.

--- dbc_overflow/db/migrate/20170817113710_create_votes.rb
+++ dbc_overflow/db/migrate/20170817113710_create_votes.rb
@@ -2,6 +2,8 @@ class CreateVotes < ActiveRecord::Migration[5.1]
   def change
     create_table :votes do |t|
       t.references :user
+      t.integer :down_vote, default: 0
+      t.integer :up_vote, default: 0
       t.string :votable_type
       t.integer :votable_id
 
